c7b3625ea1a3f6a64e7c9313975f07ce9732c9eb
S4NT1A6O <sabecerra06@misena.edu.co>
2021-07-28 21:19:08 -0500

    REALIZE LA IMPLEMENTACION DE EL NAVBAR EN LO CORRESPONDIENTE A CONTRATANTE

--- Views/Vacantes/Vacantes.php
+++ Views/Vacantes/Vacantes.php
@@ -14,60 +14,54 @@
   <link rel="stylesheet" href="<?= URL; ?>Assets/css/stylesGlobal.css" />
 </head>
   <body class="body-vacs">
-    <header id="header_menu">
-      <div class="contenedor barra">
-        <span href="#" class="content-logo">
-          <a href="javascript:window.history.back();">
-            <i class="fas fa-arrow-left" id="icono-regresar"></i>
-          </a>
-          <i class="fas fa-bars" id="icono-reponsive"></i>
-          <span href="#" class="logo-nombre">
-            <img
-              src="<?= URL; ?>Assets/img/Logo_ponslabor.png"
-              alt="PonsLabor"
-              class="logo-empresa"
-            />
-            <h2>Pons<span>Labor.</span></h2>
-          </span>
+  <header id="header_menu">
+    <div class="contenedor barra">
+      <span href="#" class="content-logo">
+        <a href="javascript:window.history.back();">
+          <i class="fas fa-arrow-left" id="icono-regresar"></i>
+        </a>
+        <i class="fas fa-bars" id="icono-reponsive"></i>
+        <span href="#" class="logo-nombre">
+          <img src="<?= URL; ?>Assets/img/Logo_ponslabor.png" alt="PonsLabor" class="logo-empresa" />
+          <h2>Pons<span>Labor.</span></h2>
         </span>
-        <nav class="nav">
-          <a href="Menu">Inicio</a>
-          <a href="Aspirante" class="active">Aspirante</a>
-          <a href="Estudios">Estudios</a>
-          <a href="Experiencia">Experiencia</a>
-          <a href="HojaVida">Hoja de vida</a>
-          <button class="switch" id="switch">
-            <i class="fas fa-sun sol"></i>
-            <i class="fas fa-moon luna"></i>
-            <span class="circulo"></span>
-          </button>
-          <div class="imagen-persona">
-            <img src="<?= URL; ?>Assets/img/Logo_ponslabor.png" alt="" />
-          </div>
-        </nav>
-      </div>
-      <div class="info-persona">
-        <h3>Edier Heraldo<br /><span>Desarrollador de software web.</span></h3>
-        <ul>
-          <li><i class="fas fa-user-circle"></i><a href="#">Perfil</a></li>
-          <li><i class="fas fa-user-edit"></i><a href="#">Editar perfil</a></li>
-          <li>
-            <i class="fas fa-sign-in-alt"></i><a href="#">Cerrar sesión</a>
-          </li>
-        </ul>
-      </div>
-      <div class="contenedor-responsive">
-        <ul class="contenedor-responsive-lista">
-          <li><a href="Menu">Inicio</a></li>
-          <li><a href="Contratante">Contratante</a></li>
-          <li><a href="Vacante">Vacante</a></li>
-          <li><a href="Aspirante">Aspirante</a></li>
-          <li><a href="HojaVida">Hoja de Vida</a></li>
-          <li><a href="Estudios">Estudios</a></li>
-          <li><a href="Experiencia">Experiencia</a></li>
-        </ul>
-      </div>
-    </header>
+      </span>
+      <nav class="nav nav_menu">
+        <a href="Menu"><i class="fas fa-home"></i>Inicio</a>
+        <a href="Contratante"><i class="fas fa-user-tie"></i>Contratante</a>
+        <a href="Vacante" ><i class="fas fa-business-time"></i>Vacante</a>
+        <a href="#" class="active"><i class="fas fa-list-alt"></i>Vacantes</a>
+        
+        <button class="switch" id="switch">
+          <i class="fas fa-sun sol"></i>
+          <i class="fas fa-moon luna"></i>
+          <span class="circulo"></span>
+        </button>
+        <div class="imagen-persona">
+          <img src="<?= URL; ?>Assets/img/Logo_ponslabor.png" alt="" />
+        </div>
+      </nav>
+    </div>
+    <div class="info-persona">
+      <h3>Edier Heraldo<br /><span>Desarrollador de software web.</span></h3>
+      <ul>
+        <li><i class="fas fa-user-edit"></i><a href="Perfil_Contratante">Editar perfil</a></li>
+        <li><i class="fas fa-user-circle"></i><a href="Perfil_Contratante">Cambiar foto</a></li>
+        <li><i class="fas fa-key"></i><a href="Recuperar_Password">Cambiar contraseña</a></li>
+        <li>
+          <i class="fas fa-sign-in-alt"></i><a href="Login">Cerrar sesión</a>
+        </li>
+      </ul>
+    </div>
+    <div class="contenedor-responsive">
+      <ul class="contenedor-responsive-lista">
+        <li><a href="Menu">Inicio</a></li>
+        <li><a href="#">Contratante</a></li>
+        <li><a href="Vacante">Vacante</a></li>
+        <li><a href="Vacantes">Vacantes</a></li>
+      </ul>
+    </div>
+  </header>
 
     <div class="content-vacs">
       <!--<div class="vacs-form">-->
@@ -195,5 +189,8 @@
         </form>
       <!--</div>-->
     </div>
+    <?php
+      require_once('./Views/Components/ScriptsJs.php');
+    ?>
   </body>
 </html>
