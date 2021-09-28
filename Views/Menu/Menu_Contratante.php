1ede34d8c1d2fa9f80b516c7292e7f851ee14687
S4NT1A6O <sabecerra06@misena.edu.co>
2021-09-28 00:37:49 -0500

    CAMBIOS DASHBOARD CONTRATANTE E IMPLEMENTACION DE REDIRECCIONAMIENTOS

--- Views/Menu/Menu_Contratante.php
+++ Views/Menu/Menu_Contratante.php
@@ -26,8 +26,7 @@
     <div id="loading">
         <div id="loading-center">
         </div>
-    </div>    
-    <!-- loader END -->
+    </div>
     <!-- wrapper -->
     <div class="wrapper">
         <!-- Menu de navegación -->
@@ -35,10 +34,49 @@
         require_once('./Views/Components/LayoutC.php');
         ?>
         <!-- Page Content  -->
-        <div id="content-page" class="content-page">
+        <div id="page-content" class="content-page">
             <div class="container">
+            <div>
+              <h3 class="mb-3 align-center">Menú Principal | Perfiles Laborales</h3>
+              <h5 class="mb-3 align-center">Bienvenido <strong><?= $_SESSION['user-data']['nombreRol'] ?></strong> <b><?= $_SESSION['user-data']['nombreUsuario'] ?></b></h5>
+            </div>
+            <br>
+            <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
+              <div class="alert alert-primary" role="alert">
+                <div class="iq-alert-icon">
+                  <i class="fas fa-business-time"></i>
+                </div>
+                <div class="iq-alert-text">
+                  <?php
+                    // Definicion de la zona horaria
+                    date_default_timezone_set('America/Bogota');
+                    // Obteniendo la fecha actual con hora, minutos y segundos en PHP
+                    echo date('d F  Y h:i:s A');?>                                                                       
+                </div>
+              </div>
+              <div class="alert alert-info" role="alert">
+                <div class="iq-alert-icon">
+                <i class="far fa-plus-square"></i>
+                </div>
+                <a href="<?= URL ?>Vacante/Vacante" class="iq-alert-text">
+                  Crear | Publicar Vacante
+                </a>
+              </div>
+              <div class="alert alert-warning" role="alert">
+                <div class="iq-alert-icon">
+                  <i class="fas fa-pen-fancy"></i>
+                </div>
+                <a href="<?= URL ?>Perfil_Contratante/Perfil_Contratante" class="iq-alert-text">
+                  Añadir Descripcion Empresarial
+                </a>
+              </div>
+
+
+
+
+              <!--
               <div class="row" id="row" name="row">
-                <!-- card 1 -->
+                 card 1 
                 <div class="col-12 col-md-6 col-xl-4">
                   <div class="card">
                     <div class="card-header">
@@ -51,11 +89,9 @@
                     </div>
                   </div>
                 </div>
-                <!-- card 2 -->
+                card 2 
                 <div class="col-12 col-md-6 col-xl-4 grid-margin grid-margin-md-0">
                   <div class="card">
-                      <img  src="<?= URL ?>Assets/img/upload.png" class="card-img-top" alt="...">
-                    
                     <div class="card-body">
                       <h5 class="card-title">Special title treatment</h5>
                       <p class="card-text mb-1">With supporting text below as a natural lead-in to additional content.</p>
@@ -63,7 +99,7 @@
                     </div>
                   </div>
                 </div>
-              </div>
+              </div> -->
             </div>
         </div>
     </div>
