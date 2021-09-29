c2bf1de44f3476b8f4ae1f0fe0f299b8e49d16f6
S4NT1A6O <sabecerra06@misena.edu.co>
2021-09-29 00:01:28 -0500

    ARREGLO CONTODAS LAS OPCIONES DE CARDS PARA PRESENTAR PERIFLES DE ASPIRANTES

--- Views/Menu/Menu_Contratante.php
+++ Views/Menu/Menu_Contratante.php
@@ -22,153 +22,214 @@
 </head>
 
 <body class="right-column-fixed" id="menu">
-    <!-- loader Start -->
-    <div id="loading">
-        <div id="loading-center">
-        </div>
-    </div>
-    <!-- wrapper -->
-    <div class="wrapper">
-        <!-- Menu de navegación -->
-        <?php
-        require_once('./Views/Components/LayoutC.php');
-        ?>
-        <!-- Page Content  -->
-        <div id="page-content" class="content-page">
-            <div class="container">
-            <div>
-              <h3 class="mb-3 align-center">Menú Contratante | Perfiles Laborales</h3>
-              <h5 class="mb-3 align-center">Bienvenido <strong><?= $_SESSION['user-data']['nombreRol'] ?></strong> <b><?= $_SESSION['user-data']['nombreUsuario'] ?></b></h5>
-            </div>
-            <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
-              <div class="alert alert-primary" role="alert">
-                <div class="iq-alert-icon">
-                  <i class="fas fa-business-time"></i>
-                </div>
-                <div class="iq-alert-text">
-                  <?php
-                    // Definicion de la zona horaria
-                    date_default_timezone_set('America/Bogota');
-                    // Obteniendo la fecha actual con hora, minutos y segundos en PHP
-                    echo date('d F  Y h:i:s A');?>                                                                       
-                </div>
-              </div>
-              <div class="alert alert-info" role="alert">
-                <div class="iq-alert-icon">
-                  <a href="<?= URL ?>Vacante/Vacante" class="iq-alert-text">
-                    <i class="far fa-plus-square"></i>
-                  </a>
-                </div>
-                <a href="<?= URL ?>Vacante/Vacante" class="iq-alert-text">
-                  <strong>
-                  Crear | Publicar Vacante
-                  </strong>                  
-                </a>
-              </div>
-              <div class="alert alert-warning" role="alert">
-                <div class="iq-alert-icon">
-                  <a href="<?= URL ?>Contratante/Perfil_Contratante" class="iq-alert-text">                  
-                    <i class="fas fa-pen-fancy"></i>
-                  </a>
-                </div>
-                <a href="<?= URL ?>Contratante/Perfil_Contratante" class="iq-alert-text">
-                  <strong>                  
-                  Añadir Descripcion Empresarial
-                  </strong>
-                </a>
-              </div>
-              <br>
-              <div class="col-sm-12 col-lg-12">
-                <div class="iq-card">
-                  <div class="iq-card-header d-flex justify-content-between">
-                    <div class="iq-header-title">
-                      <h4 class="card-title">Información usuario</h4>
-                    </div>
-                  </div>
-                  <div class="iq-card-body">
-                    <div class="card-deck">
-                      <div class="col-12 col-md-6 col-xl-5">
-                        <div class="card">
-                          <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="card">
-                        <img src="../../../assets/images/placeholder.jpg" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                      </div>
-                      <div class="card">
-                        <img src="../../../assets/images/placeholder.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                      </div>
-                      <div class="card">
-                        <img src="../../../assets/images/placeholder.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">Card title</h5>
-                          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-
-              
-
-
-
-              <!--
-              <div class="row" id="row" name="row">
-                 card 1 
-                <div class="col-12 col-md-6 col-xl-4">
-                  <div class="card">
-                    <div class="card-header">
-                      Card header Hola
-                    </div>
-                    <div class="card-body">
-                      <h5 class="card-title">Special title treatment</h5>
-                      <p class="card-text mb-1">With supporting text below as a natural lead-in to additional content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
-                </div>
-                card 2 
-                <div class="col-12 col-md-6 col-xl-4 grid-margin grid-margin-md-0">
-                  <div class="card">
-                    <div class="card-body">
-                      <h5 class="card-title">Special title treatment</h5>
-                      <p class="card-text mb-1">With supporting text below as a natural lead-in to additional content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
-                </div>
-              </div> -->
-            </div>
-        </div>
-    </div>
-    <!-- Wrapper END -->
-    <!-- Footer -->
+  <!-- loader Start -->
+  <div id="loading">
+    <div id="loading-center"></div>
+  </div>
+  <!-- wrapper -->
+  <div class="wrapper">
+    <!-- Menu de navegación -->
     <?php
-    require_once('./Views/Components/Footer.php');
+    require_once('./Views/Components/LayoutC.php');
     ?>
+    <!-- Page Content  -->
+    <div id="page-content" class="content-page">
+      <div class="container">
+        <div>
+          <h3 class="mb-3 align-center">Menú Contratante | Perfiles Laborales</h3>
+          <h5 class="mb-3 align-center">Bienvenido <strong><?= $_SESSION['user-data']['nombreRol'] ?></strong> <b><?= $_SESSION['user-data']['nombreUsuario'] ?></b></h5>
+        </div>
+        <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
+          <div class="alert alert-primary" role="alert">
+            <div class="iq-alert-icon">
+              <i class="fas fa-business-time"></i>
+            </div>
+            <div class="iq-alert-text">
+              <?php
+                // Definicion de la zona horaria
+                date_default_timezone_set('America/Bogota');
+                // Obteniendo la fecha actual con hora, minutos y segundos en PHP
+                echo date('d F  Y h:i:s A');
+              ?>                                                                       
+            </div>
+          </div>
+          <div class="alert alert-info" role="alert">
+            <div class="iq-alert-icon">
+              <a href="<?= URL ?>Vacante/Vacante" class="iq-alert-text">
+                <i class="far fa-plus-square"></i>
+              </a>
+            </div>
+            <a href="<?= URL ?>Vacante/Vacante" class="iq-alert-text">
+              <strong>
+              Crear | Publicar Vacante
+              </strong>                  
+            </a>
+          </div>
+          <div class="alert alert-warning" role="alert">
+            <div class="iq-alert-icon">
+              <a href="<?= URL ?>Contratante/Perfil_Contratante" class="iq-alert-text">                  
+                <i class="fas fa-pen-fancy"></i>
+              </a>
+            </div>
+            <a href="<?= URL ?>Contratante/Perfil_Contratante" class="iq-alert-text">
+              <strong>                  
+              Añadir Descripcion Empresarial
+              </strong>
+            </a>
+          </div>
+        </div>
+        <br>
+        <br>
+        <div class="col-12 col-xl-12 grid-margin stretch-card">
+          <br>
+            <h3>Opcion Deck con imagenes</h3>
+          <br>
+          <div class="card-deck">
+            <!-- CARDS SIN IMAGEN -->
+            <div class="col-12 col-md-5 col-xl-4">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Programador BackEnd</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to content.</p>
+                  <a href="#" class="btn btn-primary">Ver Perfil</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4">
+              <div class="card">
+                <div class="card-header">
+                  Tecnologo en gestion empresarial
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Raul Matinez</h5>
+                  <p class="card-text mb-1">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Ver Perfil</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4 grid-margin grid-margin-md-0">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Prospero Becerra</h5>
+                  <p class="card-text mb-1">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Ver Perfil</a>
+                </div>
+              </div>
+            </div>
+          </div><!-- END DIV COLUMS -->
+          <br>
+            <h3>Opcion Deck con imagenes</h3>
+          <br>
+          <div class="card-deck">                      
+            <!-- CARDS CON IMAGEN -->
+            <div class="card">
+              <img src="<?= URL ?>Assets/img/uploads/St.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Programador FrontEnd</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <a href="#" class="btn btn-primary">Ver Perfil</a>
+              </div>
+            </div>
+            <div class="card">
+              <img src="<?= URL ?>Assets/img/uploads/spacex-logo-featured.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Diseñador 3D</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <a href="#" class="btn btn-primary">Ver Perfil</a>                            
+              </div>
+            </div>
+            <div class="card">
+              <img src="<?= URL ?>Assets/img/brands/Ubi.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <a href="#" class="btn btn-primary">Ver Perfil</a>
+              </div>
+            </div>
+          </div>
+          <br>
+            <h3>Opcion con columnas y combinado</h3>
+          <br>
+          <div class="card-columns">
+            <div class="card">
+                <img src="<?= URL ?>Assets/img/uploads/St.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Programador FrontEnd</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <a href="#" class="btn btn-primary">Ver Perfil</a>
+              </div>
+            </div>
+            <div class="card p-3">
+              <blockquote class="blockquote mb-0 card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">
+                  <small class="text-muted">
+                    Someone famous in <cite title="Source Title">Source Title</cite>
+                  </small>
+                </footer>
+              </blockquote>
+            </div>
+            <div class="card">
+              <img src="<?= URL ?>Assets/img/uploads/spacex-logo-featured.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Diseñador 3D</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <a href="#" class="btn btn-primary">Ver Perfil</a>                            
+              </div>
+            </div>
+            <div class="card bg-primary text-white text-center p-3">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
+                <footer class="blockquote-footer text-white">
+                  <small>
+                    Someone famous in <cite title="Source Title">Source Title</cite>
+                  </small>
+                </footer>
+              </blockquote>
+            </div>
+            <div class="card text-center">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This card has a regular title and short paragraphy of text below it.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+            <div class="card">
+              <img src="<?= URL ?>Assets/img/brands/Ubi.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <a href="#" class="btn btn-primary">Ver Perfil</a>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Wrapper END -->
+  <!-- Footer -->
+  <?php
+  require_once('./Views/Components/Footer.php');
+  ?>
 
-    <!-- Scripts  -->
-    <?php
-    require_once('./Views/Components/ScriptsJs.php');
-    ?>
-    <!-- <script src="<?= URL ?>Assets/js/menuContratante.js"></script> -->
-    </body>
+  <!-- Scripts  -->
+  <?php
+  require_once('./Views/Components/ScriptsJs.php');
+  ?>
+  <!-- <script src="<?= URL ?>Assets/js/menuContratante.js"></script> -->
+</body>
 </html>
