e1b6df826e348900f8e19eca85aa0b0245336641
eideard-hm <ehhernandez81@misena.edu.co>
2021-10-27 10:32:03 -0500

    Se agrego el controlador de categorias

--- Controllers/Index.php
+++ Controllers/Index.php
@@ -14,21 +14,10 @@ class Index extends Controllers
         $data['titulo_pagina'] = 'Página Inicio | ' . NOMBRE_EMPRESA . '.';
         $this->views->getView($this, 'index', $data);
     }
-}
-    //======================== Controlador Categorias =======================
-
-class Categorias extends Controllers
-{
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    //======================== EVIAR Y RECIBIR INFORMACIÓN DEL MODELO =======================
 
     public function Categorias()
     {
-        $data['titulo_pagina'] = 'Categorias | ' . NOMBRE_EMPRESA . '.';
+        $data['titulo_pagina'] = 'Categorías | ' . NOMBRE_EMPRESA . '.';
         $this->views->getView($this, 'Categorias', $data);
     }
 }
