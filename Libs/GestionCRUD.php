40c9bc5e2ca78529a01378fe5df4a782f1426867
eideard-hm <ehhernandez81@misena.edu.co>
2021-11-02 10:59:15 -0500

    Se comentareo los archivos de la carpeta Libs

--- Libs/GestionCRUD.php
+++ Libs/GestionCRUD.php
@@ -7,17 +7,23 @@ class GestionCRUD extends Conexion
     private array $arrValues;
     private $conexion;
 
+    /** 
+     * Constructor de la clase, el cual inicializa la conexion a la base de datos.
+     */
     public function __construct()
     {
         $this->conexion = new Conexion();
         $this->conexion = $this->conexion->Connect();
     }
 
-    /**
-     * Métodos para realizar la crud a la base de datos
+    /** 
+     * Método para insertar registros en la base de datos.
+     * @param string $sql Consulta que se va a ejecutar.
+     * @param array $arrValues Valores que se van a insertar en la consulta.
+     * @return int $result Si la inserción fue correcta retorna el id del registro insertado, de lo contrario retorna 0,
+     * de los contrario retorna 0.
+     * @author Edier Heraldo Hernández Molano @eideard-hm
      */
-
-    //Método para insertar
     public function insert(string $sql, array $arrValues)
     {
         $this->strquery = $sql;
@@ -32,7 +38,12 @@ class GestionCRUD extends Conexion
         return $result;
     }
 
-    //Método para traer un solo elemento de la base de datos
+    /**
+     * Método para traer un solo elemento de la base de datos.
+     * @param string $sql Consulta que se va a ejecutar.
+     * @return array $result Resultado de la consulta.
+     * @author Edier Heraldo Hernández Molano @eideard-hm
+     */
     public function select(string $sql)
     {
         $this->strquery = $sql;
@@ -43,7 +54,11 @@ class GestionCRUD extends Conexion
         return $data;
     }
 
-    //Método para seleccionar todos los elementos de la base de datos
+    /** 
+     * Método para seleccionar todos los elementos de la base de datos.
+     * @param string $sql Consulta que se va a ejecutar.
+     * @return array $data Arreglo con los datos de la consulta.
+     */
     public function selectAll(String $sql)
     {
         $this->strquery = $sql;
@@ -54,27 +69,40 @@ class GestionCRUD extends Conexion
         return $data;
     }
 
-    //Método para editar
+    /** 
+     * Método para editar un registro de la base de datos.
+     * @param string $sql Consulta a la base que se va a ejecutar.
+     * @param array $arrValues Arreglo con los valores que se van a actualizar.
+     * @author Edier Heraldo Hernández Molano
+     */
     public function edit(string $sql, array $arrValues)
     {
         $this->strquery = $sql;
         $this->arrValues = $arrValues;
 
         $stmt = $this->conexion->prepare($sql);
-        $resUpdate = $stmt->execute($arrValues);
-        return $resUpdate;
+        return $stmt->execute($arrValues);
     }
 
-    //Metodo para eliminar
+    /** 
+     * Método para eliminar un elemento de la base de datos.
+     * @param string $sql Consulta a la base de datos que se va a ejecutar.
+     * @return boolean Retorna true si se ejecuta la consulta correctamente.
+     * @author Edier Heraldo Hernández Molano @eideard-hm
+     */
     public function delete(string $sql)
     {
         $this->strquery = $sql;
 
         $stmt = $this->conexion->prepare($sql);
-        $resDelete =  $stmt->execute();
-        return $resDelete;
+        return $stmt->execute();
     }
 
+    /**
+     * Método para cerrar la conexión a la base de datos, cuando se terminan
+     * los procesos
+     * @author Edier Heraldo Hernández Molano @eideard-hm
+     */
     public function __destruct()
     {
         $this->conexion = null;
